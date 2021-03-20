8b15b7fc08674149065d7f7ef7c6ffaaa3ba6bf4
hadi-cyber <hadikusumanto2000@gmail.com>
2021-03-20 21:16:28 +0700

    UPDATE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,18 +7,18 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('bootstrap/css/bootstrap.min.css') }}">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('dist/css/AdminLTE.min.css') }}">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="l"><b>My</b>Profile</a>
+    <a href="#"><b>My</b>Profile</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
